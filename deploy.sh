8cd6613e0e5ae1f87b03f8d7423404156281be89
jAdiba75 <juweriya.adiba@gmail.com>
2022-09-03 14:06:59 -0400

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+Hello, I am updating this file from dev branch.
 This file contains the deployment script.
 Hi, I am updating this script to demonstrate the git pull command and the need for it.
 updating in dev branch
