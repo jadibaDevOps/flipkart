171d233f46ef858cfd72d186272210bc790c82e2
Juweriya <112576157+jadiba75@users.noreply.github.com>
2022-09-13 15:39:07 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ This file contains the deployment script.
 Hi, I am updating this script to demonstrate the git pull command and the need for it.
 updating in dev branch
 update 2
+Updating the line to demonstrate git fetch
