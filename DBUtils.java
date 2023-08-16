d1785cebf4002eb91aa0578fd467fcbd567a2ca6
jadiba75 <juweriya.adiba@gmail.com>
2023-08-16 00:10:54 -0400

    Modified DBUtils

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,4 @@
 This java class contains the DB related code.
 Updating this file with new line.
 This is just for practice purpose.
+Updated to check if all files need to be committed to push from local to remote repo
