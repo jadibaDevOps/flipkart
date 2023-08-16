2f2b3914c0d2e5d4000097318c5ab5f77e845eef
Juweriya <112576157+jadiba75@users.noreply.github.com>
2023-08-16 16:59:46 -0400

    Delete DBUtils.java

--- DBUtils.java
+++
@@ -1,5 +0,0 @@
-I have updated again to show another feature.
-This java class contains the DB related code.
-Updating this file with new line.
-This is just for practice purpose.
-Updated to check if all files need to be committed to push from local to remote repo
