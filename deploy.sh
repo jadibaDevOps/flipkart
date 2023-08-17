3e5225e33b76676be391e54c99c1dd5e942f07b5
Juweriya <112576157+jadiba75@users.noreply.github.com>
2023-08-17 16:06:10 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ Hi, updating this line from development branch for second time.
 Updating in Development branch.
 This file contains the deployment script.
 Updated this line in GitHub GUI
+
+
+Updating to demonstrate the git pull command!
